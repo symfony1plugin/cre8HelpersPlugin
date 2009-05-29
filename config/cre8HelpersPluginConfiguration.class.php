0202c3496842fd89c784874f1b91d24c3183bf2c
tehcrow <tehcrow@3c5571db-af2c-42a9-8d5c-01322bf8fdae>
2009-05-29 11:04:09 +0000

    initial import
    git-svn-id: http://213.171.196.75/svn/cre8/projects/plugins/symfony/cre8HelpersPlugin/trunk@374 3c5571db-af2c-42a9-8d5c-01322bf8fdae

---
+++ config/cre8HelpersPluginConfiguration.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * cre8HelpersPlugin configuration.
+ * 
+ * @package     cre8HelpersPlugin
+ * @subpackage  config
+ * @author      Bogumil Wrona <b.wrona@cre8newmedia.com>
+ * @version     SVN: $Id: PluginConfiguration.class.php 12675 2008-11-06 08:07:42Z Kris.Wallsmith $
+ */
+class cre8HelpersPluginConfiguration extends sfPluginConfiguration
+{
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
